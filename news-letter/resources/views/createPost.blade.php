34d0255014ba43b74c5a6cc9833703ace43a7425
david rusycki <davidrusyck@gmail.com>
2023-04-27 22:56:30 -0300

    alterando projeto

--- news-letter/resources/views/createPost.blade.php
+++ news-letter/resources/views/createPost.blade.php
@@ -10,7 +10,53 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
             <div class="col-md-6 offset-md-3">
-                    <h1>O que há de news?</h1>
+
+                <x-form-section submit="createUser">
+                    <x-slot name="title">
+                        {{ __('O que há de news?') }}
+                    </x-slot>
+
+                    <x-slot name="description">
+                        {{ __('Cadastre uma news letter.') }}
+                    </x-slot>
+
+                    <x-slot name="form" action="/posts" method="POST" enctype="multipart/form-data">
+
+                        <!-- Titulo -->
+                        <div class="col-span-6 sm:col-span-4">
+                            <x-label for="tittle" value="{{ __('Título') }}" />
+                            <x-input id="tittle" type="text" class="mt-1 block w-full" wire:model.defer="state.name" />
+                            <x-input-error for="tittle" class="mt-2" />
+                        </div>
+
+                        <!-- Descrição -->
+                        <div class="col-span-6 sm:col-span-4">
+                            <x-label for="content" value="{{ __('Descrição') }}" />
+                            <x-input id="content" type="text" class="mt-1 block w-full" wire:model.defer="state.name" />
+                            <x-input-error for="content" class="mt-2" />
+                        </div>
+
+                        <div class="col-span-6 sm:col-span-4">
+                            @csrf
+                            <div class="form-group">
+                                <x-label for="image" value="{{ __('Imagem') }}" />
+                                <input type="file" id="image" name="image" class="form-control-file">
+                            </div>
+                        </div>
+
+                    </x-slot>
+
+                    <x-slot name="actions">
+                        <x-button>
+                            {{ __('Save') }}
+                        </x-button>
+                    </x-slot>
+                </x-form-section>
+
+
+                    <h1 style="font-size: 2em; font-weight:bold;" class="pt-4">
+                        O que há de news?
+                    </h1>
                     <form action="/posts" method="POST" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group">
--- news-letter/resources/views/posts.blade.php
+++ news-letter/resources/views/posts.blade.php
@@ -29,31 +29,38 @@
                 </head>
                 <body class="antialiased">
                     <div class="container principal">
-                        <div id="search-container" class="col-md-12">
-                            <h1>Busque uma Newsletter.</h1>
-                            {{$id}}
-                            <form action="/posts" method="GET">
-                                <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
-                            </form>
-                        </div>
                         <div style="text-align: center; margin-bottom: 1em;">    
+                            <div id="search-container" class="col-md-12">
+                                <h1 style="font-size: 2em; font-weight:bold;" class="pt-4">
+                                    Busque por uma Newsletter
+                                </h1>
+                                <form action="/posts" method="GET" class="py-4">
+                                    <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+                                </form>
+                            </div>
                             <span >
-                                Número de posts: {{count($posts)}}
+                                Número total de posts: {{count($posts)}}
                             </span>
                         </div>
                         <div class="conteudo-central row">
                             @foreach ($posts as $post)
-                                <div class="col-md-2" style="width: 22rem; margin: 1em;">
-                                    <div class="card">
-                                        <img style="height: 200px !important;" src="/img/post/{{ $post->image }}" class="card-img-top img-fluid" alt="{{ $post->title }}">
-                                        <div class="card-body" >
-                                            <h5 class="card-title">{{$post->tittle}}</h5>
-                                            <p class="card-text"><pre>{{$post->content}}</p>
-                                            <a href="{{url("/posts/{$post->id}")}}" class="btn btn-primary botaozinho">Go To</a>
-                                        </div>
-                                    </div>
+
+                            <div class="w-60 rounded overflow-hidden shadow-lg ml-6 mb-6">
+                                <img style="height: 10em" class="w-full h-24" src="/img/post/{{ $post->image }}" alt="image">
+                                <div class="px-6 py-4">
+                                    <div class="font-bold text-xl mb-2">{{$post->tittle}}</div>
+                                    <p class="text-gray-700 text-base">
+                                    {{$post->content}}
+                                    </p>
                                 </div>
-                                <br>
+                                <div class="py-4">
+                                    <a  href="{{url("/posts/{$post->id}")}}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-blue bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        Veja mais
+                                        <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                    </a>
+                                </div>
+                            </div>
+                                
                             @endforeach
                         </div>
                     </div>
